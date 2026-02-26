258caa89b5ad09b1289bea66e17ae9e7756fad44
Justin Jereza <justinjereza@gmail.com>
2026-02-27 05:39:56 +0800

    Modernized filter code.

--- lib/jekyll/rst.rb
+++ lib/jekyll/rst.rb
@@ -1,14 +1,40 @@
-require "jekyll"
+# frozen_string_literal: true
 
-root = File.expand_path('rst', File.dirname(__FILE__))
-require "#{root}/filters"
-require "#{root}/version"
-
-require File.expand_path('rst', File.dirname(__FILE__))
+require "rbst"
 
 module Jekyll
   module Rst
-    module RestConverter
+    class Error < StandardError; end
   end
+
+  module Converters
+    class Rest < Converter
+      safe true
+      priority :low
+
+      def matches(ext)
+        ext =~ /^\.rst$/i
+      end
+
+      def output_ext(ext)
+        ".html"
+      end
+
+      def convert(content)
+        RbST.new(content).to_html(initial_header_level: 2, syntax_highlight: "short")
+      end
+    end
+  end
+
+  module Rst
+    module Filters
+      def restify(input)
+        @context.registers[:site].find_converter_instance(
+          Jekyll::Converters::Rest
+        ).convert(input.to_s)
+      end
+    end
   end
 end
+
+Liquid::Template.register_filter(Jekyll::Rst::Filters)
